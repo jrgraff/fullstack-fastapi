246c2f5141f0ac1f46e806cb14dba6809dd98fea
Jackson Graff <jacksongraff@hotmail.com>
2022-08-06 09:48:51 -0300

    create api validation with pydantic validator

--- fastapi/models/share.py
+++ fastapi/models/share.py
@@ -1,4 +1,6 @@
 import ormar
+import re
+from pydantic import validator
 from database.config import database, metadata
 
 class Share(ormar.Model):
@@ -11,3 +13,9 @@ class Share(ormar.Model):
     name: str = ormar.String(max_length=100)
     abbreviation: str = ormar.String(max_length=5)
     cnpj: str = ormar.String(max_length=14)
+
+    @validator('abbreviation')
+    def abbreviation_format_validation(cls, value):
+        if not re.compile('^[A-Z]{4}[0-9]{1,2}$').match(value):
+            raise ValueError('invalid abbreviation!')
+        return value
