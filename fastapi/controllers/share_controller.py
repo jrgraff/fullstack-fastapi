3b0af360205a37fdd48f0fcac1233c775c7c0620
Jackson Graff <jacksongraff@hotmail.com>
2022-08-07 10:22:45 -0300

    feat get share by id

--- fastapi/controllers/share_controller.py
+++ fastapi/controllers/share_controller.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+import ormar
+from fastapi import APIRouter, Response
 
 from models.share import Share
 
@@ -12,3 +13,11 @@ async def show_shares():
 async def create_share(share: Share):
   await Share.save(share)
   return share
+
+@router.get("/{share_id}")
+async def get_share_by_id(share_id: int, response: Response):
+  try:
+    return await Share.objects.get(id=share_id)
+  except ormar.exceptions.NoMatch:
+    response.status_code = 404
+    return {"message": "Share not found"}
--- fastapi/tests/api/test_shares.py
+++ fastapi/tests/api/test_shares.py
@@ -1,5 +1,7 @@
+import asyncio
 from fastapi.testclient import TestClient
 
+from models.share import Share
 from tests.utils.shares import create_valid_share, create_invalid_share
 
 def test_create_share(client: TestClient) -> None:
@@ -15,3 +17,21 @@ def test_create_invalid_share(client: TestClient) -> None:
   body = create_invalid_share(['abbreviation'])
   response = client.post('/shares/', json=body)
   assert response.status_code == 422
+
+def test_get_share_by_id(client: TestClient) -> None:
+  attributes = create_valid_share()
+  share = Share(**attributes)
+  loop = asyncio.new_event_loop()
+  loop.run_until_complete(share.save())
+
+  response = client.get(f'/shares/{share.id}')
+  content = response.json()
+
+  assert response.status_code == 200
+  assert content['abbreviation'] == share.abbreviation
+
+def test_get_inexistent_share_by_id(client: TestClient) -> None:
+  response = client.get(f'/shares/404')
+  content = response.json()
+
+  assert response.status_code == 404
