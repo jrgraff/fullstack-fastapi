b432de4859f9e71df22a921ce935b24a467bd61d
Jackson <jacksongraff@hotmail.com>
2022-10-02 09:46:31 -0300

    feat: create signup and login page

--- django/sharevidz-project/sharevidz/urls.py
+++ django/sharevidz-project/sharevidz/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,6 +24,11 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    
+    # AUTH
+    path('signup', views.SignUp.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
