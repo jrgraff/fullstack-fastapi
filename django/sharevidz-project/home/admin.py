381533f31751033dabbc2d21c1e631f4502bb8f0
Jackson <jacksongraff@hotmail.com>
2022-10-02 10:33:50 -0300

    feat: create models to authenticate user

--- django/sharevidz-project/home/admin.py
+++ django/sharevidz-project/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Home, Video
 
-# Register your models here.
+admin.site.register(Home)
+admin.site.register(Video)
--- django/sharevidz-project/home/models.py
+++ django/sharevidz-project/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Home(models.Model):
+    title = models.CharField(max_length=255)
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    youtube_id = models.CharField(max_length=255)
+    hall = models.ForeignKey(Home, on_delete=models.CASCADE)
