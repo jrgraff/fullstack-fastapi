756075c322d3e391e0be28f6e0661b6eb9e36f82
Jackson Graff <jackson.graff@biopark.com.br>
2022-10-06 16:37:15 -0300

    fix: change models to link a gallery with a user

--- django/sharevidz-project/gallery/models.py
+++ django/sharevidz-project/gallery/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Gallery(models.Model):
     title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Video(models.Model):
     title = models.CharField(max_length=255)
--- django/sharevidz-project/gallery/views.py
+++ django/sharevidz-project/gallery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
@@ -20,3 +20,8 @@ class CreateGallery(generic.CreateView):
     fields = ['title']
     template_name = 'gallery/create_gallery.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateGallery, self).form_valid(form)
+        return redirect('home')
