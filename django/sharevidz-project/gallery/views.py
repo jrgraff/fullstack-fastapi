4a245147a0ce44a862c61927eb22f3b52c709a9e
Jackson Graff <jackson.graff@biopark.com.br>
2022-10-06 16:45:33 -0300

    feat: authenticate user after signup

--- django/sharevidz-project/gallery/views.py
+++ django/sharevidz-project/gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Gallery
 
@@ -13,6 +14,14 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get(
+            'username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
 
 
 class CreateGallery(generic.CreateView):
